32b53267e37fcc8fbdc739684fb9817d64ab7421
Nana Kwesi Ofosu-Aikins <nanaaikinson2020@gmail.com>
2024-02-25 08:32:56 +0000

    Feature: env WIP

---
+++ env/env.go
@@ -0,0 +1,64 @@
+package gofacades
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+type env struct{}
+
+func Env() *env {
+	return &env{}
+}
+
+// GetString gets string type env from os.
+func (e *env) GetString(key string, defaultValue ...interface{}) string {
+	return e.getEnv(key, defaultValue...).(string)
+}
+
+// GetInt gets int type env from os.
+func (e *env) GetInt(key string, defaultValue ...interface{}) int {
+	val := e.getEnv(key, defaultValue...)
+	switch v := val.(type) {
+	case int:
+		return v
+	case string:
+		intVal, err := strconv.Atoi(v)
+		if err == nil {
+			return intVal
+		}
+	}
+	return 0
+}
+
+// GetBool gets bool type config from application.
+func (e *env) GetBool(key string, defaultValue ...interface{}) bool {
+	val := e.getEnv(key, defaultValue...)
+	switch v := val.(type) {
+	case bool:
+		return v
+	case string:
+		boolVal, err := strconv.ParseBool(v)
+		if err == nil {
+			return boolVal
+		}
+	}
+	return false
+}
+
+// GetStringSlice gets string from env, split it by comma and converts to string slice.
+func (e *env) GetStringSlice(key string, defaultValue ...interface{}) []string {
+	val := e.getEnv(key, defaultValue...)
+	split := strings.Split(val.(string), ",")
+
+	return split
+}
+
+func (e *env) getEnv(key string, defaultValue ...interface{}) interface{} {
+	val := os.Getenv(key)
+	if val == "" && len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+	return val
+}
